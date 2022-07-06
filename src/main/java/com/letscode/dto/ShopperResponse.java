9f8c9ab4245317ff0b414523aa200a68f4d1cce3
yurimarch <ypdmarch@gmail.com>
2022-07-07 00:06:11 +0200

    ShopperResponse added

--- src/main/java/com/letscode/dto/ShopperResponse.java
+++ src/main/java/com/letscode/dto/ShopperResponse.java
@@ -1,4 +1,27 @@
 package com.letscode.dto;
 
+import com.letscode.model.Shopper;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter
+@NoArgsConstructor
+@ToString
 public class ShopperResponse {
+
+    private String id;
+    private String name;
+    private String email;
+    private String phoneNumber;
+    private String cpf;
+
+    public ShopperResponse(Shopper shopper){
+        this.setId(shopper.getId());
+        this.setName(shopper.getName());
+        this.setEmail(shopper.getEmail());
+        this.setPhoneNumber(shopper.getPhoneNumber());
+        this.setCpf(shopper.getCpf());
+    }
 }
